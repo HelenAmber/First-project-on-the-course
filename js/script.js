38465519fb26e01378165f872d38a54f3f2a84b9
Elena <elena.nisman85@gmail.com>
2022-01-21 14:38:07 +0200

    Add block Timer, create function getTimeRemaining

--- js/script.js
+++ js/script.js
@@ -106,4 +106,24 @@ new Card (
   '.menu .container'
  ).render();
 
+ // Timer
+
+ const deadLine = '2022-03-18';
+
+ function getTimeRemaining(endtime){
+     const t = Date.parse(endtime) - Date.parse(new Date()),
+           days = Math.floor(t/(1000 * 60 * 60 * 24)),
+           hours = Math.floor(t/(1000 * 60 * 60) % 24),
+           minutes = Math.floor(t/(1000 * 60) % 60),
+           seconds = Math.floor((t/1000) % 60);
+
+     return {
+         'total': t,
+         'days': days,
+         'hours': hours,
+         'minutes': minutes,
+         'seconds': seconds
+         };
+ }
+
 });
