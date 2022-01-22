055227c9ba84257145f104386bec98c905771215
Elena <elena.nisman85@gmail.com>
2022-01-22 21:33:14 +0200

    add function getDynamicInformation

--- js/script.js
+++ js/script.js
@@ -159,8 +159,6 @@ function setClock(selector, endtime){
 }
 setClock('.timer', deadLine);
 
-});
-
 // Modal
 const modalTrigger = document.querySelectorAll('[data-modal]'),
       modal = document.querySelector('.modal');
@@ -413,3 +411,35 @@ function getStaticInformation(selector, activeClass){
 
 getStaticInformation('#gender div', 'calculating__choose-item_active');
 getStaticInformation('.calculating__choose_big div', 'calculating__choose-item_active');
+
+function getDynamicInformation(selector){
+  const input = document.querySelector(selector);
+
+  input.addEventListener('input', () => {
+
+    if(input.value.match(/\D/g)){
+       input.style.border = '2px solid red';
+    } else {
+       input.style.border = 'none';
+    }
+
+    switch(input.getAttribute('id')) {
+        case 'height':
+          height = +input.value;
+          break;
+        case 'weight':
+          weight = +input.value;
+          break;
+        case 'age':
+          age = +input.value;
+          break;
+    }
+    calcTotal();
+  });
+}
+
+getDynamicInformation('#height');
+getDynamicInformation('#weight');
+getDynamicInformation('#age');
+
+});
